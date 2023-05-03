net, scm: fix PaX detected msg_controllen overflow in scm_detach_fds

example/cnn_text_classification/data_helpers.py
@@ -79,4 +79,83 @@ def build_input_data(sentences, labels, vocabulary):
     """
     Maps sentencs and labels to vectors based on a vocabulary.
     """
-    x = np.arra
+    x = np.array([[vocabulary[word] for word in sentence] for sentence in sentences])
+    y = np.array(labels)
+    return [x, y]
+
+def build_input_data_with_word2vec(sentences, labels, word2vec):
+    """Map sentences and labels to vectors based on a pretrained word2vec"""
+    x_vec = []
+    for sent in sentences:
+        vec = []
+        for word in sent:
+            if word in word2vec:
+                vec.append(word2vec[word])
+            else:
+                vec.append(word2vec['</s>'])
+        x_vec.append(vec)
+    x_vec = np.array(x_vec)
+    y_vec = np.array(labels)
+    return [x_vec, y_vec]
+
+
+def load_data_with_word2vec(word2vec):
+    """
+    Loads and preprocessed data for the MR dataset.
+    Returns input vectors, labels, vocabulary, and inverse vocabulary.
+    """
+    # Load and preprocess data
+    sentences, labels = load_data_and_labels()
+    sentences_padded = pad_sentences(sentences)
+    # vocabulary, vocabulary_inv = build_vocab(sentences_padded)
+    return build_input_data_with_word2vec(sentences_padded, labels, word2vec)
+
+
+def load_data():
+    """
+    Loads and preprocessed data for the MR dataset.
+    Returns input vectors, labels, vocabulary, and inverse vocabulary.
+    """
+    # Load and preprocess data
+    sentences, labels = load_data_and_labels()
+    sentences_padded = pad_sentences(sentences)
+    vocabulary, vocabulary_inv = build_vocab(sentences_padded)
+    x, y = build_input_data(sentences_padded, labels, vocabulary)
+    return [x, y, vocabulary, vocabulary_inv]
+
+
+def batch_iter(data, batch_size, num_epochs):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_data = data[shuffle_indices]
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+
+
+def load_pretrained_word2vec(infile):
+    if isinstance(infile, str):
+        infile = open(infile)
+
+    word2vec = {}
+    for idx, line in enumerate(infile):
+        if idx == 0:
+            vocab_size, dim = line.strip().split()
+        else:
+            tks = line.strip().split()
+            word2vec[tks[0]] = map(float, tks[1:])
+
+    return word2vec
+
+
+def load_google_word2vec(path):
+    model = word2vec.Word2Vec.load_word2vec_format(path, binary=True)
+    return model