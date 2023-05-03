984905842aca29211ce461ca07f5fed4b2432afb
dolphin2003 <dolphin2003@proton.me>
2023-05-03 16:57:40 +0000

    Add coveralls integration

--- example/caffe/train_model.py
+++ example/caffe/train_model.py
@@ -52,4 +52,49 @@ def fit(args, network, data_loader, eval_metrics=None, batch_end_callback=None):
     epoch_size = args.num_examples / args.batch_size
 
     if args.kv_store == 'dist_sync':
-        e
+        epoch_size /= kv.num_workers
+        model_args['epoch_size'] = epoch_size
+
+    if 'lr_factor' in args and args.lr_factor < 1:
+        model_args['lr_scheduler'] = mx.lr_scheduler.FactorScheduler(
+            step = max(int(epoch_size * args.lr_factor_epoch), 1),
+            factor = args.lr_factor)
+
+    if 'clip_gradient' in args and args.clip_gradient is not None:
+        model_args['clip_gradient'] = args.clip_gradient
+
+    # disable kvstore for single device
+    if 'local' in kv.type and (
+            args.gpus is None or len(args.gpus.split(',')) is 1):
+        kv = None
+
+    model = mx.model.FeedForward(
+        ctx                = devs,
+        symbol             = network,
+        num_epoch          = args.num_epochs,
+        learning_rate      = args.lr,
+        momentum           = 0.9,
+        wd                 = 0.00001,
+        initializer        = mx.init.Xavier(factor_type="in", magnitude=2.34),
+        **model_args)
+
+    if eval_metrics == None:
+        eval_metrics = ['accuracy']
+        ## TopKAccuracy only allows top_k > 1
+        for top_k in [5, 10, 20]:
+            eval_metrics.append(mx.metric.create('top_k_accuracy', top_k = top_k))
+
+    if batch_end_callback is not None:
+        if not isinstance(batch_end_callback, list):
+            batch_end_callback = [batch_end_callback]
+    else:
+        batch_end_callback = []
+    batch_end_callback.append(mx.callback.Speedometer(args.batch_size, 50))
+
+    model.fit(
+       X                  = train,
+       eval_data          = val,
+       eval_metric        = eval_metrics,
+       kvstore            = kv,
+       batch_end_callback = batch_end_callback,
+       epoch_end_callback = checkpoint)
