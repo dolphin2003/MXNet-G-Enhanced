aafb60b104c29f7504e9c5c3173dc49f8588812f
dolphin2003 <dolphin2003@proton.me>
2023-08-03 15:11:00 +0000

    Updated Travis CI configuration

--- example/rcnn/helper/dataset/pascal_voc.py
+++ example/rcnn/helper/dataset/pascal_voc.py
@@ -45,4 +45,165 @@ class PascalVOC(IMDB):
 
         self.config = {'comp_id': 'comp4',
                        'use_diff': False,
-                       'm
+                       'min_size': 2}
+
+    @property
+    def cache_path(self):
+        """
+        make a directory to store all caches
+        :return: cache path
+        """
+        cache_path = os.path.join(self.root_path, 'cache')
+        if not os.path.exists(cache_path):
+            os.mkdir(cache_path)
+        return cache_path
+
+    def load_image_set_index(self):
+        """
+        find out which indexes correspond to given image set (train or val)
+        :return:
+        """
+        image_set_index_file = os.path.join(self.data_path, 'ImageSets', 'Main', self.image_set + '.txt')
+        assert os.path.exists(image_set_index_file), 'Path does not exist: {}'.format(image_set_index_file)
+        with open(image_set_index_file) as f:
+            image_set_index = [x.strip() for x in f.readlines()]
+        return image_set_index
+
+    def image_path_from_index(self, index):
+        """
+        given image index, find out full path
+        :param index: index of a specific image
+        :return: full path of this image
+        """
+        image_file = os.path.join(self.data_path, 'JPEGImages', index + '.jpg')
+        assert os.path.exists(image_file), 'Path does not exist: {}'.format(image_file)
+        return image_file
+
+    def gt_roidb(self):
+        """
+        return ground truth image regions database
+        :return: imdb[image_index]['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        """
+        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb.pkl')
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as fid:
+                roidb = cPickle.load(fid)
+            print '{} gt roidb loaded from {}'.format(self.name, cache_file)
+            return roidb
+
+        gt_roidb = [self.load_pascal_annotation(index) for index in self.image_set_index]
+        with open(cache_file, 'wb') as fid:
+            cPickle.dump(gt_roidb, fid, cPickle.HIGHEST_PROTOCOL)
+        print 'wrote gt roidb to {}'.format(cache_file)
+
+        return gt_roidb
+
+    def load_pascal_annotation(self, index):
+        """
+        for a given index, load image and bounding boxes info from XML file
+        :param index: index of a specific image
+        :return: record['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        """
+        import xml.etree.ElementTree as ET
+        filename = os.path.join(self.data_path, 'Annotations', index + '.xml')
+
+        tree = ET.parse(filename)
+        objs = tree.findall('object')
+        if not self.config['use_diff']:
+            non_diff_objs = [obj for obj in objs if int(obj.find('difficult').text) == 0]
+            objs = non_diff_objs
+        num_objs = len(objs)
+
+        boxes = np.zeros((num_objs, 4), dtype=np.uint16)
+        gt_classes = np.zeros((num_objs), dtype=np.int32)
+        overlaps = np.zeros((num_objs, self.num_classes), dtype=np.float32)
+
+        class_to_index = dict(zip(self.classes, range(self.num_classes)))
+        # Load object bounding boxes into a data frame.
+        for ix, obj in enumerate(objs):
+            bbox = obj.find('bndbox')
+            # Make pixel indexes 0-based
+            x1 = float(bbox.find('xmin').text) - 1
+            y1 = float(bbox.find('ymin').text) - 1
+            x2 = float(bbox.find('xmax').text) - 1
+            y2 = float(bbox.find('ymax').text) - 1
+            cls = class_to_index[obj.find('name').text.lower().strip()]
+            boxes[ix, :] = [x1, y1, x2, y2]
+            gt_classes[ix] = cls
+            overlaps[ix, cls] = 1.0
+
+        overlaps = scipy.sparse.csr_matrix(overlaps)
+
+        return {'boxes': boxes,
+                'gt_classes': gt_classes,
+                'gt_overlaps': overlaps,
+                'flipped': False}
+
+    def roidb(self, gt_roidb):
+        return self.selective_search_roidb(gt_roidb)
+
+    def load_selective_search_roidb(self, gt_roidb):
+        """
+        turn selective search proposals into selective search roidb
+        :param gt_roidb: [image_index]['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        :return: roidb: [image_index]['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        """
+        matfile = os.path.join(self.root_path, 'selective_search_data', self.name + '.mat')
+        assert os.path.exists(matfile), 'selective search data does not exist: {}'.format(matfile)
+        raw_data = scipy.io.loadmat(matfile)['boxes'].ravel()  # original was dict ['images', 'boxes']
+
+        box_list = []
+        for i in range(raw_data.shape[0]):
+            boxes = raw_data[i][:, (1, 0, 3, 2)] - 1  # pascal voc dataset starts from 1.
+            keep = unique_boxes(boxes)
+            boxes = boxes[keep, :]
+            keep = filter_small_boxes(boxes, self.config['min_size'])
+            boxes = boxes[keep, :]
+            box_list.append(boxes)
+
+        return self.create_roidb_from_box_list(box_list, gt_roidb)
+
+    def selective_search_roidb(self, gt_roidb):
+        """
+        get selective search roidb and ground truth roidb
+        :param gt_roidb: ground truth roidb
+        :return: roidb of selective search (ground truth included)
+        """
+        cache_file = os.path.join(self.cache_path, self.name + '_ss_roidb.pkl')
+        if os.path.exists(cache_file):
+            with open(cache_file, 'rb') as fid:
+                roidb = cPickle.load(fid)
+            print '{} ss roidb loaded from {}'.format(self.name, cache_file)
+            return roidb
+
+        if self.image_set != 'test':
+            ss_roidb = self.load_selective_search_roidb(gt_roidb)
+            roidb = IMDB.merge_roidbs(gt_roidb, ss_roidb)
+        else:
+            roidb = self.load_selective_search_roidb(None)
+        with open(cache_file, 'wb') as fid:
+            cPickle.dump(roidb, fid, cPickle.HIGHEST_PROTOCOL)
+        print 'wrote ss roidb to {}'.format(cache_file)
+
+        return roidb
+
+    def load_rpn_roidb(self, gt_roidb):
+        """
+        turn rpn detection boxes into roidb
+        :param gt_roidb: [image_index]['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        :return: roidb: [image_index]['boxes', 'gt_classes', 'gt_overlaps', 'flipped']
+        """
+        rpn_file = os.path.join(self.root_path, 'rpn_data', self.name + '_rpn.pkl')
+        print 'loading {}'.format(rpn_file)
+        assert os.path.exists(rpn_file), 'rpn data not found at {}'.format(rpn_file)
+        with open(rpn_file, 'rb') as f:
+            box_list = cPickle.load(f)
+        return self.create_roidb_from_box_list(box_list, gt_roidb)
+
+    def rpn_roidb(self, gt_roidb):
+        """
+        get rpn roidb and ground truth roidb
+        :param gt_roidb: ground truth roidb
+        :return: roidb of rpn (ground truth included)
+        """
+  
