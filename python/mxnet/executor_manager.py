1a606de4e4ce71071f4e123fc5e672a65db185b1
dolphin2003 <dolphin2003@proton.me>
2023-10-20 21:39:34 +0000

    ru.po: Updated Russian translation.

--- python/mxnet/executor_manager.py
+++ python/mxnet/executor_manager.py
@@ -107,4 +107,153 @@ def _bind_exec(sym, ctx, input_shapes, param_names, need_grad=False,
 
     arg_names = sym.list_arguments()
 
-    if nee
+    if need_grad is False:
+        need_grad = set()
+    elif need_grad is True:
+        need_grad = set(arg_names) - set(input_shapes.keys())
+    elif isinstance(need_grad, set):
+        pass
+    else:
+        raise AssertionError("need_grad must be boolean or set.")
+    grad_req = {name:('write' if name in need_grad else 'null') for name in arg_names}
+
+
+    # create or borrow arguments and gradients
+    for i, name in enumerate(arg_names):
+        if not name in param_names:
+            # data or label
+            if shared_data_arrays is not None and \
+                    name in shared_data_arrays:
+                arg_arr = shared_data_arrays[name]
+
+                if np.prod(arg_arr.shape) >= np.prod(arg_shape[i]):
+                    # good, we can share this memory
+                    assert(arg_types[i] == arg_arr.dtype)
+                    arg_arr = arg_arr.reshape(arg_shape[i])
+                else:
+                    logger.warning(('bucketing: data "%s" has a shape %s' % (name, arg_shape[i])) +
+                                   (', which is larger than already allocated ') +
+                                   ('shape %s' % (arg_arr.shape,)) +
+                                   ('. Need to re-allocate. Consider putting ') +
+                                   ('default_bucket_key to be the bucket taking the largest ') +
+                                   ('input for better memory sharing.'))
+                    arg_arr = nd.zeros(arg_shape[i], ctx, dtype=arg_types[i])
+
+                    # replace existing shared array because the new one is bigger
+                    shared_data_arrays[name] = arg_arr
+            else:
+                arg_arr = nd.zeros(arg_shape[i], ctx, dtype=arg_types[i])
+                if shared_data_arrays is not None:
+                    shared_data_arrays[name] = arg_arr
+
+            arg_arrays.append(arg_arr)
+        else:
+            # model parameter
+            if base_exec is None:
+                arg_arr = nd.zeros(arg_shape[i], ctx, dtype=arg_types[i])
+                if name in need_grad:
+                    grad_arr = nd.zeros(arg_shape[i], ctx, dtype=arg_types[i])
+                    grad_arrays[name] = grad_arr
+            else:
+                arg_arr = base_exec.arg_dict[name]
+                assert arg_arr.shape == arg_shape[i]
+                assert arg_arr.dtype == arg_types[i]
+                if name in need_grad:
+                    grad_arrays[name] = base_exec.grad_dict[name]
+            arg_arrays.append(arg_arr)
+
+    # create or borrow aux variables
+    if base_exec is None:
+        aux_arrays = [nd.zeros(s, ctx, dtype=t) for s, t in zip(aux_shape, aux_types)]
+    else:
+        for i, a in enumerate(base_exec.aux_arrays):
+            assert aux_shape[i] == a.shape
+            assert aux_types[i] == a.dtype
+
+        aux_arrays = [a for a in base_exec.aux_arrays]
+
+    executor = sym.bind(ctx=ctx, args=arg_arrays, args_grad=grad_arrays,
+                        aux_states=aux_arrays,
+                        grad_req=grad_req, shared_exec=base_exec)
+    return executor
+
+class DataParallelExecutorGroup(object):
+    """A group of executors living on different devices, for data parallelization.
+
+    Parameters
+    ----------
+    sym: Symbol
+        The network configuration.
+    arg_names: list of str
+        Equals `sym.list_arguments()`
+    param_names: list of str
+        List of names of all trainable parameters.
+    ctx: list of Context
+        List of devices for training (data parallelization)
+    slices: list of int
+        Describes how the data parallelization splits data into different devices.
+    train_data: DataIter (or DataBatch)
+        The dataset for training. It could be any object with `provide_data` and
+        `provide_label` properties. Loading of actual data is not necessarily needed
+        at this stage.
+    shared_grop: DataParallelExecutorGroup
+        An existing executor group, if to share parameters with it.
+    """
+    def __init__(self, sym, arg_names, param_names, ctx, slices, train_data, shared_group=None):
+        # make sure the architecture is valid
+        _check_arguments(sym)
+
+        if shared_group is None:
+            self.shared_data_arrays = [{} for _ in ctx]
+        else:
+            self.shared_data_arrays = shared_group.shared_data_arrays
+
+        self.data_names = [x[0] for x in train_data.provide_data]
+        self.label_names = [x[0] for x in train_data.provide_label]
+        self.aux_names = sym.list_auxiliary_states()
+        self.param_idx = [i for i in range(len(arg_names)) if arg_names[i] in param_names]
+        self.param_names = [arg_names[i] for i in self.param_idx]
+
+        self.train_execs = []
+        for i, ctxi in enumerate(ctx):
+            data_shapes = {k: tuple([slices[i].stop-slices[i].start] + list(v[1:]))
+                           for k, v in train_data.provide_data + train_data.provide_label}
+            shared_exec = None if shared_group is None else shared_group.train_execs[i]
+            train_exec = _bind_exec(sym, ctxi, data_shapes, self.param_names,
+                                    need_grad=True, base_exec=shared_exec,
+                                    shared_data_arrays=self.shared_data_arrays[i])
+            self.train_execs.append(train_exec)
+
+        # data structure
+        self.data_arrays = [[(slices[i], e.arg_dict[name]) for i, e in enumerate(self.train_execs)]
+                            for name in self.data_names]
+        self.label_arrays = [[(slices[i], e.arg_dict[name]) for i, e in enumerate(self.train_execs)]
+                             for name in self.label_names]
+
+        self.param_arrays = [[e.arg_arrays[i] for e in self.train_execs]
+                             for i in self.param_idx]
+        self.grad_arrays = [[e.grad_arrays[i] for e in self.train_execs]
+                            for i in self.param_idx]
+
+        self.aux_arrays = [[e.aux_arrays[i] for e in self.train_execs]
+                           for i in range(len(self.aux_names))]
+
+        self.slices = slices
+
+    def load_data_batch(self, data_batch):
+        """ load data and labels into arrays """
+        _load_data(data_batch, self.data_arrays)
+        _load_label(data_batch, self.label_arrays)
+
+    def forward(self, is_train=False):
+        """ Perform a forward pass on each executor """
+        for texec in self.train_execs:
+            texec.forward(is_train=is_train)
+
+    def backward(self):
+        """ Perform a backward pass on each executor """
+        for texec in self.train_execs:
+            texec.backward()
+
+    def update_metric(self, metric, labels):
+        """ Update evaluation metric with label and cu
