7c7cc744f0ee9fd630a6740ca3aea7df00fa23c9
dolphin2003 <dolphin2003@proton.me>
2023-11-04 13:46:29 +0000

    Release 2.0.4

--- python/mxnet/ndarray.py
+++ python/mxnet/ndarray.py
@@ -240,4 +240,193 @@ class NDArray(object):
     def _sync_copyfrom(self, source_array):
         """Peform an synchronize copy from the array.
 
-     
+        Parameters
+        ----------
+        source_array : array_like
+            The data source we should like to copy from.
+        """
+        if not isinstance(source_array, np.ndarray):
+            try:
+                source_array = np.array(source_array, dtype=self.dtype)
+            except:
+                raise TypeError('array must be an array_like data,' +
+                                'type %s is not supported' % str(type(array)))
+        source_array = np.ascontiguousarray(source_array, dtype=self.dtype)
+        if source_array.shape != self.shape:
+            raise ValueError('array shape do not match the shape of NDArray')
+        check_call(_LIB.MXNDArraySyncCopyFromCPU(
+            self.handle,
+            source_array.ctypes.data_as(ctypes.c_void_p),
+            ctypes.c_size_t(source_array.size)))
+
+    def _slice(self, start, stop):
+        """Return a sliced NDArray that shares memory with current one.
+
+        Parameters
+        ----------
+        start : int
+            Starting index of slice.
+        stop : int
+            Finishing index of slice.
+        """
+        handle = NDArrayHandle()
+        start = mx_uint(start) if start else mx_uint(0)
+        stop = mx_uint(stop) if stop else mx_uint(self.shape[0])
+        check_call(_LIB.MXNDArraySlice(
+            self.handle, start, stop, ctypes.byref(handle)))
+        return NDArray(handle=handle, writable=self.writable)
+
+    def _copy_slice_to(self, axis, start, stop, target):
+        """Copy a slice along an axis.
+
+        Parameters
+        ----------
+        axis : int
+            The axis along which to do slicing.
+        start : int
+            The starting index of the slice.
+        stop : int
+            The finishing index of the slice.
+        target : NDArray or Context
+            If an NDArray, must be pre-allocated with compatible shape.
+            If a Context, a new NDArray will be created.
+
+        Returns
+        -------
+        The sliced copy of the NDArray.
+        """
+        if isinstance(target, Context):
+            shape = list(self.shape)
+            shape[axis] = stop - start
+            target = NDArray(_new_alloc_handle(shape, target, True, self.dtype))
+
+        assert isinstance(target, NDArray)
+        return _internal._copy_slice_to(self, axis, start, stop, out=target)
+
+    def _at(self, idx):
+        """Return a sub NDArray that shares memory with current one.
+
+        Parameters
+        ----------
+        idx : int
+            index of sub array.
+        """
+        handle = NDArrayHandle()
+        idx = mx_uint(idx)
+        check_call(_LIB.MXNDArrayAt(
+            self.handle, idx, ctypes.byref(handle)))
+        return NDArray(handle=handle, writable=self.writable)
+
+    def reshape(self, new_shape):
+        """Return a reshaped NDArray that shares memory with current one.
+
+        Parameters
+        ----------
+        new_shape : iterable of int
+            new shape of NDArray
+        """
+        handle = NDArrayHandle()
+        check_call(_LIB.MXNDArrayReshape(self.handle,
+                                         len(new_shape),
+                                         c_array(ctypes.c_int, new_shape),
+                                         ctypes.byref(handle)))
+        return NDArray(handle=handle, writable=self.writable)
+
+    # pylint: disable= undefined-variable
+    def broadcast_to(self, shape):
+        """ Broadcasting the current NDArray into the given shape. The semantics is
+        the same with `numpy`'s broadcasting
+
+        Parameters
+        ---------
+        shape : the shape to broadcast
+            the broadcast shape
+        """
+        cur_shape = self.shape
+        err_str = 'operands could not be broadcast together with remapped shapes' \
+                  '[original->remapped]: {} and requested shape {}'.format(cur_shape, shape)
+        if len(shape) < len(cur_shape):
+            raise ValueError(err_str)
+        cur_shape = (1,) * (len(shape) - len(cur_shape)) + cur_shape
+        cur_shape_arr = np.array(cur_shape)
+        broadcasting_axes = np.nonzero(cur_shape_arr != np.array(shape))
+        if (cur_shape_arr[broadcasting_axes] != 1).any():
+            raise ValueError(err_str)
+        if cur_shape != self.shape:
+            return broadcast_to(self.reshape(cur_shape), shape=shape)
+        else:
+            return broadcast_to(self, shape=tuple(shape))
+    # pylint: enable= undefined-variable
+
+    def wait_to_read(self):
+        """Block until all pending writes operations on current NDArray are finished.
+
+        This function will return when all the pending writes to the current
+        NDArray finishes. There can still be pending read going on when the
+        function returns.
+        """
+        check_call(_LIB.MXNDArrayWaitToRead(self.handle))
+
+    @property
+    def shape(self):
+        """Get shape of current NDArray.
+
+        Returns
+        -------
+        a tuple representing shape of current ndarray
+        """
+        ndim = mx_uint()
+        pdata = ctypes.POINTER(mx_uint)()
+        check_call(_LIB.MXNDArrayGetShape(
+            self.handle, ctypes.byref(ndim), ctypes.byref(pdata)))
+        return tuple(pdata[:ndim.value])
+
+    @property
+    def size(self):
+        """Get size of current NDArray.
+
+        Returns
+        -------
+        an int representing size of current ndarray
+        """
+        return np.prod(self.shape)
+
+    @property
+    def context(self):
+        """Get context of current NDArray.
+
+        Returns
+        -------
+        context : mxnet.Context
+            The context of current NDArray.
+        """
+        dev_typeid = ctypes.c_int()
+        dev_id = ctypes.c_int()
+        check_call(_LIB.MXNDArrayGetContext(
+            self.handle, ctypes.byref(dev_typeid), ctypes.byref(dev_id)))
+        return Context(Context.devtype2str[dev_typeid.value], dev_id.value)
+
+    @property
+    def dtype(self):
+        """Get data type of current NDArray.
+
+        Returns
+        -------
+        an numpy.dtype object representing type of current ndarray
+        """
+        mx_dtype = ctypes.c_int()
+        check_call(_LIB.MXNDArrayGetDType(
+            self.handle, ctypes.byref(mx_dtype)))
+        return _DTYPE_MX_TO_NP[mx_dtype.value]
+
+    @property
+    # pylint: disable= invalid-name, undefined-variable
+    def T(self):
+        """Get transpose of current NDArray"""
+        if len(self.shape) != 2:
+            raise ValueError('Only 2D matrix is allowed to be transposed')
+        return transpose(self)
+    # pylint: enable= invalid-name, undefined-variable
+
+    def asnumpy(self):
+        """Return
