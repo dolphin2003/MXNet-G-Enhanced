b336454f0d87e377a42d3ec5c7a09870059b62d1
dolphin2003 <dolphin2003@proton.me>
2023-12-11 14:26:31 +0000

    Miscellaneous non-functional changes (spelling, etc).

--- scala-package/init/src/main/scala/ml/dmlc/mxnet/init/Base.scala
+++ scala-package/init/src/main/scala/ml/dmlc/mxnet/init/Base.scala
@@ -19,4 +19,17 @@ object Base {
   type SymbolHandle = CPtrAddress
 
   @throws(classOf[UnsatisfiedLinkError])
-  private def tryLoadInitLibrary(): Unit
+  private def tryLoadInitLibrary(): Unit = {
+    val baseDir = System.getProperty("user.dir") + "/init-native"
+    val os = System.getProperty("os.name")
+    // ref: http://lopica.sourceforge.net/os.html
+    if (os.startsWith("Linux")) {
+      System.load(s"$baseDir/linux-x86_64/target/libmxnet-init-scala-linux-x86_64.so")
+    } else if (os.startsWith("Mac")) {
+      System.load(s"$baseDir/osx-x86_64/target/libmxnet-init-scala-osx-x86_64.jnilib")
+    } else {
+      // TODO(yizhi) support windows later
+      throw new UnsatisfiedLinkError()
+    }
+  }
+}
