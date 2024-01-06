dedeeef3bf80e8073433cf8af17a4ae92bebd45c
dolphin2003 <dolphin2003@proton.me>
2024-01-06 20:25:12 +0000

    rapidio/rionet: fix multicast packet transmit logic

---
+++ src/operator/batch_norm-inl.h
@@ -0,0 +1,51 @@
+/*!
+ * Copyright (c) 2015 by Contributors
+ * \file batch_norm-inl.h
+ * \brief
+ * \author Bing Xu
+*/
+#ifndef MXNET_OPERATOR_BATCH_NORM_INL_H_
+#define MXNET_OPERATOR_BATCH_NORM_INL_H_
+
+#include <dmlc/logging.h>
+#include <dmlc/parameter.h>
+#include <mxnet/operator.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+#include "./operator_common.h"
+#include "./mshadow_op.h"
+
+namespace mxnet {
+namespace op {
+
+namespace batchnorm {
+enum BatchNormOpInputs {kData, kGamma, kBeta};
+enum BatchNormOpOutputs {kOut, kMean, kVar};
+enum BatchNormOpAuxiliary {kMovingMean, kMovingVar};
+enum BatchNormBackResource {kTempSpace};
+}  // namespace batchnorm
+
+struct BatchNormParam : public dmlc::Parameter<BatchNormParam> {
+  float eps;
+  float momentum;
+  bool fix_gamma;
+  bool use_global_stats;
+  DMLC_DECLARE_PARAMETER(BatchNormParam) {
+    DMLC_DECLARE_FIELD(eps).set_default(1e-3f)
+    .describe("Epsilon to prevent div 0");
+    DMLC_DECLARE_FIELD(momentum).set_default(0.9f)
+    .describe("Momentum for moving average");
+    DMLC_DECLARE_FIELD(fix_gamma).set_default(true)
+    .describe("Fix gamma while training");
+    DMLC_DECLARE_FIELD(use_global_stats).set_default(false)
+    .describe("Whether use global moving statistics instead of local batch-norm. "
+              "This will force change batch-norm into a scale shift operator.");
+  }
+};
+
+template<typename xpu>
+class BatchNormOp : public Operator {
+ public:
+  explicit BatchNormOp(BatchNormPar
