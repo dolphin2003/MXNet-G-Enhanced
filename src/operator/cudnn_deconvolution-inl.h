75636e191b692c6ab76ca3c1d8d475e02b3ba114
dolphin2003 <dolphin2003@proton.me>
2024-01-14 10:26:12 +0000

    Update base

--- src/operator/cudnn_deconvolution-inl.h
+++ src/operator/cudnn_deconvolution-inl.h
@@ -158,4 +158,135 @@ class CuDNNDeconvolutionOp : public Operator {
       CHECK_EQ(cudnnConvolutionBackwardFilter_v3(s->dnn_handle_,
                &alpha,
                out_desc_,
-               grad.dptr_ + out_offset_ 
+               grad.dptr_ + out_offset_ * g,
+               in_desc_,
+               data.dptr_ + data_offset_ * g,
+               conv_desc_,
+               back_algo_w_,
+               workspace.dptr_,
+               backward_workspace_byte_,
+               &beta,
+               filter_desc_,
+               gwmat.dptr_ + weight_offset_ * g), CUDNN_STATUS_SUCCESS);
+      #elif CUDNN_MAJOR == 5
+      CHECK_EQ(cudnnConvolutionBackwardFilter(s->dnn_handle_,
+               &alpha,
+               out_desc_,
+               grad.dptr_ + out_offset_ * g,
+               in_desc_,
+               data.dptr_ + data_offset_ * g,
+               conv_desc_,
+               back_algo_w_,
+               workspace.dptr_,
+               backward_workspace_byte_,
+               &beta,
+               filter_desc_,
+               gwmat.dptr_ + weight_offset_ * g), CUDNN_STATUS_SUCCESS);
+      #endif
+      CHECK_EQ(cudnnConvolutionForward(s->dnn_handle_,
+                                       &alpha,
+                                       out_desc_,
+                                       grad.dptr_ + out_offset_ * g,
+                                       filter_desc_,
+                                       wmat.dptr_ + weight_offset_ * g,
+                                       conv_desc_,
+                                       algo_,
+                                       workspace.dptr_,
+                                       forward_workspace_byte_,
+                                       &beta,
+                                       in_desc_,
+                                       gdata.dptr_ + data_offset_ * g), CUDNN_STATUS_SUCCESS);
+    }
+  }
+
+ private:
+  inline void Init(mshadow::Stream<gpu> *s,
+                   const std::vector<TBlob> &in_data,
+                   const std::vector<TBlob> &out_data) {
+    using namespace mshadow;
+    #if CUDNN_MAJOR == 5
+    format_ = CUDNN_TENSOR_NCHW;
+    #endif
+    size_t expected = param_.no_bias ? 2 : 3;
+    CHECK_EQ(in_data.size(), expected);
+    CHECK_EQ(out_data.size(), 1);
+    if (!init_cudnn_) {
+      init_cudnn_ = true;
+      size_t workspace_byte = static_cast<size_t>(param_.workspace * sizeof(DType));
+      size_t back_size = 0;
+      size_t back_size_w = 0;
+      Tensor<gpu, 4, DType> data = in_data[deconv::kData].get<gpu, 4, DType>(s);
+      Tensor<gpu, 4, DType> out = out_data[deconv::kOut].get<gpu, 4, DType>(s);
+      index_t pad_y, pad_x, adj_y, adj_x;
+      param_.InferPad(data.size(2), data.size(3), &pad_y, &pad_x, &adj_y, &adj_x);
+      data_offset_ = data.shape_[1] / param_.num_group * data.shape_[2] * data.shape_[3];
+      out_offset_ = out.shape_[1] /param_.num_group * out.shape_[2] * out.shape_[3];
+      weight_offset_ = data.shape_[1] / param_.num_group * param_.num_filter / param_.num_group
+                       * param_.kernel[0] * param_.kernel[1];
+      CHECK_EQ(cudnnCreateTensorDescriptor(&in_desc_), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnCreateTensorDescriptor(&out_desc_), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnCreateTensorDescriptor(&bias_desc_), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnCreateFilterDescriptor(&filter_desc_), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnCreateConvolutionDescriptor(&conv_desc_), CUDNN_STATUS_SUCCESS);
+      #if CUDNN_MAJOR <=4
+      CHECK_EQ(cudnnSetFilter4dDescriptor(filter_desc_,
+                                          dtype_,
+                                          data.shape_[1] / param_.num_group,
+                                          param_.num_filter / param_.num_group,
+                                          param_.kernel[0],
+                                          param_.kernel[1]), CUDNN_STATUS_SUCCESS);
+      #elif CUDNN_MAJOR ==5
+      CHECK_EQ(cudnnSetFilter4dDescriptor(filter_desc_,
+                                          dtype_,
+                                          format_,
+                                          data.shape_[1] / param_.num_group,
+                                          param_.num_filter / param_.num_group,
+                                          param_.kernel[0],
+                                          param_.kernel[1]), CUDNN_STATUS_SUCCESS);
+      #endif
+      CHECK_EQ(cudnnSetConvolution2dDescriptor(conv_desc_,
+                                               pad_y,
+                                               pad_x,
+                                               param_.stride[0],
+                                               param_.stride[1],
+                                               1,
+                                               1,
+                                               CUDNN_CROSS_CORRELATION), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnSetTensor4dDescriptorEx(in_desc_,
+                                            dtype_,
+                                            data.shape_[0],
+                                            data.shape_[1] / param_.num_group,
+                                            data.shape_[2],
+                                            data.shape_[3],
+                                            data.shape_[1] * data.shape_[2] * data.shape_[3],
+                                            data.shape_[2] * data.shape_[3],
+                                            data.shape_[3],
+                                            1), CUDNN_STATUS_SUCCESS);
+      CHECK_EQ(cudnnSetTensor4dDescriptorEx(out_desc_,
+                                            dtype_,
+                                            out.shape_[0],
+                                            out.shape_[1] / param_.num_group,
+                                            out.shape_[2],
+                                            out.shape_[3],
+                                            out.shape_[1] * out.shape_[2] * out.shape_[3],
+                                            out.shape_[2] * out.shape_[3],
+                                            out.shape_[3],
+                                            1), CUDNN_STATUS_SUCCESS);
+      if (!param_.no_bias) {
+        Tensor<gpu, 1, DType> bias = in_data[deconv::kBias].get<gpu, 1, DType>(s);
+        bias_offset_ = bias.shape_[0] / param_.num_group;
+        CHECK_EQ(cudnnSetTensor4dDescriptor(bias_desc_,
+                                            CUDNN_TENSOR_NCHW,
+                                            dtype_,
+                                            1,
+                                            bias.shape_[0] / param_.num_group,
+                                            1,
+                                            1), CUDNN_STATUS_SUCCESS);
+      }
+      CHECK_EQ(s->dnn_handle_ownership_, mshadow::Stream<gpu>::OwnHandle);
+      CHECK_EQ(cudnnGetConvolutionForwardAlgorithm(s->dnn_handle_,
+               out_desc_,
+               filter_desc_,
+               conv_desc_,
+               in_desc_,
+               CUDNN_
