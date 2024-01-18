6786ef3201fd80774bfef687884ef4e16cfbb7be
dolphin2003 <dolphin2003@proton.me>
2024-01-18 15:49:35 +0000

    MAGETWO-35090: Implement SalesIncrement module

--- src/operator/elementwise_binary_op-inl.h
+++ src/operator/elementwise_binary_op-inl.h
@@ -52,4 +52,148 @@ void PlusBackward_(const OutputGrad& out_grad,
   mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
   MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
       mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
-      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_g
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad, F<mshadow_op::identity>(mout_grad));
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad, F<mshadow_op::identity>(mout_grad));
+    });
+}
+
+template<typename xpu>
+void MinusBackward_(const OutputGrad& out_grad,
+                    const EnvArguments& env,
+                    TBlob* lhs_grad,
+                    TBlob* rhs_grad,
+                    OpReqType req_lhs_grad,
+                    OpReqType req_rhs_grad,
+                    RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad, F<mshadow_op::identity>(mout_grad));
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad, F<mshadow_op::negation>(mout_grad));
+    });
+}
+
+template<typename xpu>
+void MulBackward_(const OutputGrad& out_grad,
+                  const Input0& lhs,
+                  const Input1& rhs,
+                  const EnvArguments& env,
+                  TBlob* lhs_grad,
+                  TBlob* rhs_grad,
+                  OpReqType req_lhs_grad,
+                  OpReqType req_rhs_grad,
+                  RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_data = lhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_data = rhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      CHECK_NE(req_rhs_grad, kWriteInplace);
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad, mlhs_data * mout_grad);
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad, mrhs_data * mout_grad);
+    });
+}
+
+template<typename xpu>
+void DivBackward_(const OutputGrad& out_grad,
+                  const Input0& lhs,
+                  const Input1& rhs,
+                  const EnvArguments& env,
+                  TBlob* lhs_grad,
+                  TBlob* rhs_grad,
+                  OpReqType req_lhs_grad,
+                  OpReqType req_rhs_grad,
+                  RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_data = lhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_data = rhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      CHECK_NE(req_rhs_grad, kWriteInplace);
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad,
+                      F<mshadow_op::negation>(mout_grad * mlhs_data)/
+                      F<mshadow_op::square>(mrhs_data));
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad, mout_grad /  mrhs_data);
+    });
+}
+
+template<typename xpu>
+void PowerBackward_(const OutputGrad& out_grad,
+                    const Input0& lhs,
+                    const Input1& rhs,
+                    const EnvArguments& env,
+                    TBlob* lhs_grad,
+                    TBlob* rhs_grad,
+                    OpReqType req_lhs_grad,
+                    OpReqType req_rhs_grad,
+                    RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_data = lhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_data = rhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      CHECK_NE(req_rhs_grad, kWriteInplace);
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad,
+                      F<mshadow_op::log>(mlhs_data) *
+                      F<mshadow_op::power>(mlhs_data, mrhs_data) * mout_grad);
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad,
+                      mrhs_data *
+                      F<mshadow_op::power>(mlhs_data, mrhs_data - scalar<DType>(1)) *
+                      mout_grad);
+    });
+}
+
+template<typename xpu>
+void MaximumBackward_(const OutputGrad& out_grad,
+                      const Input0& lhs,
+                      const Input1& rhs,
+                      const EnvArguments& env,
+                      TBlob* lhs_grad,
+                      TBlob* rhs_grad,
+                      OpReqType req_lhs_grad,
+                      OpReqType req_rhs_grad,
+                      RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_data = lhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_data = rhs.data.FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mlhs_grad = lhs_grad->FlatTo1D<xpu, DType>(s);
+      mshadow::Tensor<xpu, 1, DType> mrhs_grad = rhs_grad->FlatTo1D<xpu, DType>(s);
+      CHECK_NE(req_rhs_grad, kWriteInplace);
+      ASSIGN_DISPATCH(mrhs_grad, req_rhs_grad,
+                      mout_grad * F<mshadow_op::maximum_grad>(mrhs_data, mlhs_data));
+      ASSIGN_DISPATCH(mlhs_grad, req_lhs_grad,
+                      mout_grad * F<mshadow_op::maximum_grad>(mlhs_data, mrhs_data));
+    });
+}
+
+template<typename xpu>
+void MinimumBackward_(const OutputGrad& out_grad,
+                      const Input0& lhs,
+                      const Input1& rhs,
+                      const EnvArguments& env,
+                      TBlob* lhs_grad,
+                      TBlob* rhs_grad,
+                      OpReqType req_lhs_grad,
+                      OpReqType req_rhs_grad,
+                      RunContext ctx) {
+  using namespace mshadow::expr;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(lhs_grad->type_flag_, DType, {
+      mshadow::Tensor<xpu, 1, DType> mout_grad = out_grad.data.FlatT
