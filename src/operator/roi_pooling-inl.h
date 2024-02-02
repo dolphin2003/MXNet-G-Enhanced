3833a0f272f4d2a218fc5c3afb1818d9e189e199
dolphin2003 <dolphin2003@proton.me>
2024-02-02 06:23:47 +0000

    Various fixes and improvements.

--- src/operator/roi_pooling-inl.h
+++ src/operator/roi_pooling-inl.h
@@ -117,4 +117,106 @@ class ROIPoolingOp : public Operator {
   ROIPoolingParam param_;
 };  // class ROIPoolingOp
 
-// Decalre Factory
+// Decalre Factory function, used for dispatch specialization
+template<typename xpu>
+Operator* CreateOp(ROIPoolingParam param, int dtype);
+
+#if DMLC_USE_CXX11
+class ROIPoolingProp : public OperatorProperty {
+ public:
+  std::vector<std::string> ListArguments() const override {
+    return {"data", "rois"};
+  }
+
+  std::vector<std::string> ListOutputs() const override {
+    return {"output", "maxidx"};
+  }
+
+  int NumOutputs() const override {
+    return 2;
+  }
+
+  int NumVisibleOutputs() const override {
+    return 1;
+  }
+
+  void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) override {
+    param_.Init(kwargs);
+  }
+
+  std::map<std::string, std::string> GetParams() const override {
+    return param_.__DICT__();
+  }
+
+  bool InferShape(std::vector<TShape> *in_shape,
+                  std::vector<TShape> *out_shape,
+                  std::vector<TShape> *aux_shape) const override {
+    using namespace mshadow;
+    CHECK_EQ(in_shape->size(), 2) << "Input:[data, rois]";
+
+    // data: [batch_size, c, h, w]
+    TShape dshape = in_shape->at(roipool::kData);
+    CHECK_EQ(dshape.ndim(), 4) << "data should be a 4D tensor";
+
+    // bbox: [num_rois, 5]
+    TShape bshape = in_shape->at(roipool::kBox);
+    CHECK_EQ(bshape.ndim(), 2) << "bbox should be a 2D tensor of shape [batch, 5]";
+    CHECK_EQ(bshape[1], 5) << "bbox should be a 2D tensor of shape [batch, 5]";
+
+    // out: [num_rois, c, pooled_h, pooled_w]
+    // max_idx: [num_rois, c, pooled_h, pooled_w]
+    out_shape->clear();
+    out_shape->push_back(
+         Shape4(bshape[0], dshape[1], param_.pooled_size[0], param_.pooled_size[1]));
+    out_shape->push_back(
+         Shape4(bshape[0], dshape[1], param_.pooled_size[0], param_.pooled_size[1]));
+    return true;
+  }
+
+  bool InferType(std::vector<int> *in_type,
+                 std::vector<int> *out_type,
+                 std::vector<int> *aux_type) const override {
+    CHECK_EQ(in_type->size(), 2);
+    int dtype = (*in_type)[0];
+    CHECK_EQ(dtype, (*in_type)[1]);
+    CHECK_NE(dtype, -1) << "Input must have specified type";
+
+    out_type->clear();
+    out_type->push_back(dtype);
+    out_type->push_back(dtype);
+    return true;
+  }
+
+  OperatorProperty* Copy() const override {
+    ROIPoolingProp* roi_pooling_sym = new ROIPoolingProp();
+    roi_pooling_sym->param_ = this->param_;
+    return roi_pooling_sym;
+  }
+
+  std::string TypeString() const override {
+    return "ROIPooling";
+  }
+
+  // decalre dependency and inplace optimization options
+  std::vector<int> DeclareBackwardDependency(
+    const std::vector<int> &out_grad,
+    const std::vector<int> &in_data,
+    const std::vector<int> &out_data) const override {
+    return {out_grad[roipool::kOut], in_data[roipool::kBox], out_data[roipool::kMaxIdx]};
+  }
+
+  Operator* CreateOperator(Context ctx) const override {
+    LOG(FATAL) << "Not Implemented.";
+    return NULL;
+  }
+
+  Operator* CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
+                             std::vector<int> *in_type) const override;
+
+ private:
+  ROIPoolingParam param_;
+};  // class ROIPoolingProp
+#endif
+}  // namespace op
+}  // namespace mxnet
+#endif  // MXNET_OPERATOR_ROI_POOLING_INL_H_
---
+++ src/operator/sequence_last.cu
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2015 by Contributors
+ * \file sequence_last.cu
+ * \brief
+ * \author Sebastian Bodenstein
+*/
+
+#include "./sequence_last-
