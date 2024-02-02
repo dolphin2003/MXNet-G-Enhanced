705fd74e6f50347ba53b28b7da78ac4dd3a5df44
dolphin2003 <dolphin2003@proton.me>
2024-02-02 15:24:27 +0000

    Update rghost.rb

--- src/operator/sequence_last.cu
+++ src/operator/sequence_last.cu
@@ -5,4 +5,16 @@
  * \author Sebastian Bodenstein
 */
 
-#include "./sequence_last-
+#include "./sequence_last-inl.h"
+
+namespace mxnet {
+namespace op {
+template <> Operator *CreateOp<gpu>(SequenceLastParam param, int dtype) {
+  Operator *op = NULL;
+  MSHADOW_REAL_TYPE_SWITCH(dtype, DType,
+                           { op = new SequenceLastOp<gpu, DType>(param); })
+  return op;
+}
+
+}  // namespace op
+}  // namespace mxnet
